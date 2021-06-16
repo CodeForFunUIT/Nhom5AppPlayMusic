88ca30d7b82091fc5345b42fc2ae54dbea2230de
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2021-06-16 11:02:54 +0700

    19521931_Giao diện phần tìm kiếm

--- app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Tim_Kiem.java
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Tim_Kiem.java
@@ -1,22 +1,63 @@
 package com.hieunghia.dmt.appnghenhac.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.hieunghia.dmt.appnghenhac.R;
 
 public class Fragment_Tim_Kiem extends Fragment {
     View view;
+    Toolbar toolbar;
+    RecyclerView recyclerViewsearchbaihat;
+    TextView txtKhongcodulieu;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_tim_kiem, container, false);
+        toolbar = view.findViewById(R.id.toobarsearchbaihat);
+        recyclerViewsearchbaihat = view.findViewById(R.id.recyclerviewsearchbaihat);
+        txtKhongcodulieu = view.findViewById(R.id.textviewkhongcodulieu);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle("");
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_view, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("BBB", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
